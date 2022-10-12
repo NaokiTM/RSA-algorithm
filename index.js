285aaecccbaa3f4b1036ff3a9dc337cbd0f66c9e
naoki~ <ntsome21@gmail.com>
2022-10-12 11:04:55 +0100

    changed variablenames as didnt work last time

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const p = 3 document.getElementById("")//first prime number
 const q = 11 document.getElementById("")//second prime number
-const phi = 1.61803398875 //golden ratio
 const areCoprimes = (num1, num2) => {
   const smaller = num1 > num2 ? num1 : num2;
   for (let ind = 2; ind < smaller; ind ++) {
@@ -16,19 +15,19 @@ let e = document.getElementById().value
 
 function encrypt {
     let n = p*q //n = 33 (3 X 11)
-    let phin = (p - 1) * (q - 1)
+    let r = (p - 1) * (q - 1)
 
-    if (e > 1 || e < phin) {
-      if ((areCoprimes(e,phin)) == true) {
+    if (e > 1 || e < r) {
+      if ((areCoprimes(e,r)) == true) {
           let e = 7
           let d = document.getElementById().value
 
-          if ((d*e) % phin = 1) {
+          if ((d*e) % r = 1) {
             
           } else {
             console.log("not a valid value for d")
           }
-      } else {
+    } else {
           console.log("not a valid value for e")
       }
     } 
